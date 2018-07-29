14f30d2f3ddb56085b6807b40778ff75ca8dbb3b
dragon4147 <dragonlyj@gmail.com>
2018-07-29 18:14:53 +0800

    add custom pretty printer

---
+++ SSFP/AttributeGrammer/CustomPrinter.cs
@@ -0,0 +1,15 @@
+﻿namespace SimpleType
+{
+    public partial class PrettyPrinter
+    {
+        static PrettyPrinter()
+        {
+            checking = new[] {".", "^"};
+        }
+        static partial void CustomRender(string s)
+        {
+            Backup();
+            buffer.Append(s);
+        }
+    }
+}
